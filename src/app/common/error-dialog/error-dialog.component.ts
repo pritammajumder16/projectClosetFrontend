c7488c957b07788acf6adcddd35d111b0623739d
pritam16 <pritammajumder416@gmail.com>
2024-08-07 03:38:07 +0530

    Footer redesign

--- src/app/common/error-dialog/error-dialog.component.ts
+++ src/app/common/error-dialog/error-dialog.component.ts
@@ -4,12 +4,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-error-dialog',
   templateUrl: './error-dialog.component.html',
-  styleUrl: './error-dialog.component.scss'
+  styleUrl: './error-dialog.component.scss',
 })
 export class ErrorDialogComponent {
   constructor(
-    public dialogRef:MatDialogRef<ErrorDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data:any
+    public dialogRef: MatDialogRef<ErrorDialogComponent>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   onClose(): void {
