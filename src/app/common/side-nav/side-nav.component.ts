3cbd4a473eb26fcffba866165241815294fafdf7
pritam16 <pritammajumder416@gmail.com>
2024-08-07 03:30:06 +0530

    Side nav redesign

--- src/app/common/side-nav/side-nav.component.ts
+++ src/app/common/side-nav/side-nav.component.ts
@@ -5,17 +5,30 @@ import { AuthServiceService } from '../../services/auth-service.service';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.scss'
+  styleUrl: './side-nav.component.scss',
 })
 export class SideNavComponent {
   public userName: string | undefined;
-  public isAuthenticated: Boolean | undefined;
+  public isAuthenticated: boolean | undefined;
   private subscription: Subscription | undefined;
   constructor(private _authService: AuthServiceService) {}
+  public navItems = [
+    { icon: 'shopping_cart', label: 'Project Closet', action: 'closeDrawer' },
+    { icon: 'info', label: 'About Us', link: '#' },
+    { icon: 'rss_feed', label: 'Blog', link: '#' },
+    { icon: 'phone_in_talk', label: 'Contact Us', link: '#' },
+    { icon: 'help', label: 'Help & Support', link: '#' },
+    {
+      icon: 'admin_panel_settings',
+      label: 'Administration',
+      routerLink: '/admin/home',
+    },
+    { icon: 'logout', label: 'Logout', action: 'logout' },
+  ];
   ngOnInit() {
-    this.authenticator()
+    this.authenticator();
   }
-  public authenticator(){
+  public authenticator() {
     const isAuth = this._authService.getIsAuthenticated();
     if (isAuth) {
       this.isAuthenticated = isAuth;
@@ -28,10 +41,10 @@ export class SideNavComponent {
       }
     );
   }
-  logout(){
-    this._authService.logout()
+  logout() {
+    this._authService.logout();
   }
-  ngOnDestroy(){
-    this.subscription?.unsubscribe()
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
   }
 }
